c7df5c8175ee8b3d79b5d5f8d75b1768c9a6bfb6
mr-samsul-alam <contactsamsulalam@gmail.com>
2021-09-13 21:28:00 +0600

    css style

--- js/app.js
+++ js/app.js
@@ -25,8 +25,8 @@ const showProducts = (products) => {
       <h4>Rating:<b> ${product.rating.rate} </b></h4>
       <h4> <b> ${product.rating.count}</b> Person rated this product</h4>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
+      <button id="details-btn" class="btn btn-warning">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
